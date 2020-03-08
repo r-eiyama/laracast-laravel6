1d8aafd61194d3b486c5e2737145736952247c26
yamee-dev <ryo.eiyama@icloud.com>
2020-03-08 16:03:52 +0900

    Finish Section10 Views 44 Email Markdown layout

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Mail\ContactMe;
+use App\Mail\Contact;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -26,7 +26,7 @@ class ContactController extends Controller
 //            $message->to(request('email'))->subject('Hello There');
 //        });
 
-        Mail::to(request('email'))->send(new ContactMe('shirts'));
+        Mail::to(request('email'))->send(new Contact());
 
         return redirect('/contact')
             ->with('message', 'Email sent !');
